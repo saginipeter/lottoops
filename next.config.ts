ef63c04b7f5230d4d25dbf94b2b19ae7b54efb2c
Peter Sagini <88707506+WakandaKid@users.noreply.github.com>
2026-07-06 09:13:05 +0300

    up29

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // Vercel Blob
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+
+      // (Optional) Keep UploadThing if you still use old images
       {
         protocol: "https",
         hostname: "**.ufs.sh",
